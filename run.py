a6ecb29d805f5bdbb1ad8d28b6e84eebd9700ea9
amazakura <amazakura0804@gmail.com>
2020-01-06 23:28:31 +0900

    run.py

--- run.py
+++ run.py
@@ -45,7 +45,7 @@ class AIRLINIA(commands.Bot):
 
 if __name__ == '__main__':
     technetium = TECHNETIUM(command_prefix='te!')
-    asyncio.run(technetium.run(os.environ['TECHNETIUM_DISCORD_TOKEN']))
+    technetium.run(os.environ['TECHNETIUM_DISCORD_TOKEN'])
 
     airlinia = AIRLINIA(command_prefix='al!')
     asyncio.run(airlinia.run(os.environ['AIRLINIA_DISCORD_TOKEN']))
