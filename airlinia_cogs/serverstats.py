ad85ad8a839b0e8276cc1409c37365361ed9c7bc
amazakura <amazakura0804@gmail.com>
2020-01-08 05:46:08 +0900

    バグ修正

--- airlinia_cogs/serverstats.py
+++ airlinia_cogs/serverstats.py
@@ -17,16 +17,16 @@ class Server_Stats(commands.Cog):
         self.offline_channel_id = 663297305847398421
         self.message_channel_id = 663297421417119754
         self.time_channel_id = 663297453621116988
-        self.datas = json.load(open('./data/pokemon.json', 'r'))
+        self.datas = json.load(open("./data/pokemon.json", "r"))
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
         datas = self.datas
         server = member.guild
-        datas['all'] = len(server.members)
-        datas['member'] = len([member for member in server.members if not member.bot])
-        datas['bot'] = len([member for member in server.members if member.bot])
-        with open('./data/pokemon.json', "w") as file:
+        datas["all"] = len(server.members)
+        datas["member"] = len([member for member in server.members if not member.bot])
+        datas["bot"] = len([member for member in server.members if member.bot])
+        with open("./data/pokemon.json", "w") as file:
             json.dump(file, datas, indent=4)
         await channel_name_edit()
 
@@ -36,8 +36,8 @@ class Server_Stats(commands.Cog):
         server = message.guild
         if message.author.bot:  # ボットのメッセージをハネる
             return
-        datas['message'] += 1
-        with open('./data/pokemon.json', "w") as file:
+        datas["message"] += 1
+        with open("./data/pokemon.json", "w") as file:
             json.dump(file, datas, indent=4)
         await channel_name_edit()
 
@@ -45,11 +45,11 @@ class Server_Stats(commands.Cog):
     async def on_member_updata(self, before, after):
         datas = self.datas
         server = after.guild
-        datas['online'] = len([member for member in server.members if member.status == discord.Status.online])
-        datas['idle'] = len([member for member in server.members if member.status.idle])
-        datas['dnd'] = len([member for member in server.members if member.status.dnd])
-        datas['offline'] = len([member for member in server.members if member.status.offline])
-        with open('./data/pokemon.json', "w") as file:
+        datas["online"] = len([member for member in server.members if member.status == discord.Status.online])
+        datas["idle"] = len([member for member in server.members if member.status.idle])
+        datas["dnd"] = len([member for member in server.members if member.status.dnd])
+        datas["offline"] = len([member for member in server.members if member.status.offline])
+        with open("./data/pokemon.json", "w") as file:
             json.dump(file, datas, indent=4)
         await channel_name_edit()
 
@@ -65,15 +65,15 @@ class Server_Stats(commands.Cog):
         self.message_channel : discord.VoiceChannel = self.bot.get_channel(self.message_channel_id)
         self.time_channel : discord.VoiceChannel = self.bot.get_channel(self.time_channel_id)
 
-        await self.all_channel.edit(name=f'all : {data["all"]}')
-        await self.member_channel.edit(name=f'member : {data["member"]}')
-        await self.bot_channel.edit(name=f'bot : {data["bot"]}')
-        await self.online_channel.edit(name=f'online : {data["online"]}')
-        await self.idle_channel.edit(name=f'idle : {data["idle"]}')
-        await self.dnd_channel.edit(name=f'dnd : {data["dmd"]}')
-        await self.offline_channel.edit(name=f'offline : {data["offline"]}')
-        await self.message_channel.edit(name=f'message : {data["message"]}')
-        await self.time.all_channel.edit(name=f'time : {data["time"]}')
+        await self.all_channel.edit(name=f"all : {data["all"]}")
+        await self.member_channel.edit(name=f"member : {data["member"]}")
+        await self.bot_channel.edit(name=f"bot : {data["bot"]}")
+        await self.online_channel.edit(name=f"online : {data["online"]}")
+        await self.idle_channel.edit(name=f"idle : {data["idle"]}")
+        await self.dnd_channel.edit(name=f"dnd : {data["dmd"]}")
+        await self.offline_channel.edit(name=f"offline : {data["offline"]}")
+        await self.message_channel.edit(name=f"message : {data["message"]}")
+        await self.time.all_channel.edit(name=f"time : {data["time"]}")
 
 def setup(airlinia):
     airlinia.add_cog(Server_Stats(airlinia))
