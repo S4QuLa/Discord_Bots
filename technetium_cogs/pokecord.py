1adf525765b82806b99a7e6902a8a70bf579ee0f
amazakura <amazakura0804@gmail.com>
2020-01-07 23:17:11 +0900

    修正

--- technetium_cogs/pokecord.py
+++ technetium_cogs/pokecord.py
@@ -30,8 +30,8 @@ class Discord_Game_Bot(commands.Cog):
     async def on_message(self, message):
         if message.channel.id != 663148351197413396:
             return
-        print(message.attachments.url)
-        r = requests.get(message.attachments.url)
+        print(message.attachments.url[0])
+        r = requests.get(message.attachments.url[0])
         if r.status_code == 200:
             hash = imagehash.dhash(Image.open(BytesIO(r.content)))
             f = open('./data/pokemon.json')
