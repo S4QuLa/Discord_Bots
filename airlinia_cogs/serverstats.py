412d36cfd79c0aab08d01facc3180af384f7bc8e
amazakura <amazakura0804@gmail.com>
2020-01-08 04:17:27 +0900

    修正

--- airlinia_cogs/serverstats.py
+++ airlinia_cogs/serverstats.py
@@ -23,28 +23,6 @@ class Server_Stats(commands.Cog):
         self.time_channel_id = 663297453621116988
         self.data = load_json("./data/stats.json")
 
-    async def channel_name_edit(server_id):
-        data = self.data
-        self.all_channel : discord.VoiceChannel = self.bot.get_channel(self.all_channel_id)
-        self.member_channel : discord.VoicetChannel = self.bot.get_channel(self.member_channel_id)
-        self.bot_channel : discord.VoiceChannel = self.bot.get_channel(self.bot_channel_id)
-        self.online_channel : discord.VoiceChannel = self.bot.get_channel(self.online_channel_id)
-        self.idle_channel : discord.VoiceChannel = self.bot.get_channel(self.idle_channel_id)
-        self.dnd_channel : discord.VoiceChannel = self.bot.get_channel(self.dnd_channel_id)
-        self.offline_channel : discord.VoiceChannel = self.bot.get_channel(self.offline_channel_id)
-        self.message_channel : discord.VoiceChannel = self.bot.get_channel(self.message_channel_id)
-        self.time_channel : discord.VoiceChannel = self.bot.get_channel(self.time_channel_id)
-
-        await self.all_channel.edit(name=f'all : {data[server_id]['all']}')
-        await self.member_channel.edit(name=f'member : {data[server_id]['member']}')
-        await self.bot_channel.edit(name=f'bot : {data[server_id]['bot']}')
-        await self.online_channel.edit(name=f'online : {data[server_id]['online']}')
-        await self.idle_channel.edit(name=f'idle : {data[server_id]['idle']}')
-        await self.dnd_channel.edit(name=f'dnd : {data[server_id]['dmd']}')
-        await self.offline_channel.edit(name=f'offline : {data[server_id]['offline']}')
-        await self.message_channel.edit(name=f'message : {data[server_id]['message']}')
-        await self.time.all_channel.edit(name=f'time : {data[server_id]['time']}')
-
     @commands.Cog.listener()
     async def on_member_join(self, member: discord.Member, server: discord.Member.Guild):
         datas[str(server.id)]['all'] = len(server.members)
@@ -73,6 +51,28 @@ class Server_Stats(commands.Cog):
         with open(path, "r") as file:
             json.dump(file, datas, indent=4)
         await channel_name_edit(server.id)
+
+    async def channel_name_edit(server_id):
+        data = self.data
+        self.all_channel : discord.VoiceChannel = self.bot.get_channel(self.all_channel_id)
+        self.member_channel : discord.VoicetChannel = self.bot.get_channel(self.member_channel_id)
+        self.bot_channel : discord.VoiceChannel = self.bot.get_channel(self.bot_channel_id)
+        self.online_channel : discord.VoiceChannel = self.bot.get_channel(self.online_channel_id)
+        self.idle_channel : discord.VoiceChannel = self.bot.get_channel(self.idle_channel_id)
+        self.dnd_channel : discord.VoiceChannel = self.bot.get_channel(self.dnd_channel_id)
+        self.offline_channel : discord.VoiceChannel = self.bot.get_channel(self.offline_channel_id)
+        self.message_channel : discord.VoiceChannel = self.bot.get_channel(self.message_channel_id)
+        self.time_channel : discord.VoiceChannel = self.bot.get_channel(self.time_channel_id)
+
+        await self.all_channel.edit(name=f'all : {data[server_id]['all']}')
+        await self.member_channel.edit(name=f'member : {data[server_id]['member']}')
+        await self.bot_channel.edit(name=f'bot : {data[server_id]['bot']}')
+        await self.online_channel.edit(name=f'online : {data[server_id]['online']}')
+        await self.idle_channel.edit(name=f'idle : {data[server_id]['idle']}')
+        await self.dnd_channel.edit(name=f'dnd : {data[server_id]['dmd']}')
+        await self.offline_channel.edit(name=f'offline : {data[server_id]['offline']}')
+        await self.message_channel.edit(name=f'message : {data[server_id]['message']}')
+        await self.time.all_channel.edit(name=f'time : {data[server_id]['time']}')
 
 def setup(airlinia):
     airlinia.add_cog(Server_Stats(airlinia))
