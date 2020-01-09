0f2f39311b1d7c1dd88d2fbcde5195b322c1e9a3
amazakura <amazakura0804@gmail.com>
2020-01-10 01:55:05 +0900

    修正

--- airlinia_cogs/serverstats.py
+++ airlinia_cogs/serverstats.py
@@ -7,21 +7,13 @@ import json
 
 def json_load(path):
     with open(path, "r") as f:
-        return json.load(f)
+        f = str(f).replace("'", '"').replace('True', 'true').replace('False', 'false')
+        return json.loads(f)
 
 class Server_Stats(commands.Cog):
     def __init__(self, airlinia):
         self.bot = airlinia #botを受け取る。
         self.datas = json_load("./data/pokemon.json")
-        self.all_channel_id = 663297143909515274
-        self.member_channel_id = 663297196531253249
-        self.bot_channel_id = 663297233453842452
-        self.online_channel_id = 663297268455309332
-        self.idle_channel_id = 664160147886833678
-        self.dnd_channel_id = 664160201125003295
-        self.offline_channel_id = 663297305847398421
-        self.message_channel_id = 663297421417119754
-        self.time_channel_id = 663297453621116988
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
@@ -59,15 +51,15 @@ class Server_Stats(commands.Cog):
 
     async def channel_name_edit():
         datas = self.datas
-        self.all_channel : discord.VoiceChannel = self.bot.get_channel(self.all_channel_id)
-        self.member_channel : discord.VoiceChannel = self.bot.get_channel(self.member_channel_id)
-        self.bot_channel : discord.VoiceChannel = self.bot.get_channel(self.bot_channel_id)
-        self.online_channel : discord.VoiceChannel = self.bot.get_channel(self.online_channel_id)
-        self.idle_channel : discord.VoiceChannel = self.bot.get_channel(self.idle_channel_id)
-        self.dnd_channel : discord.VoiceChannel = self.bot.get_channel(self.dnd_channel_id)
-        self.offline_channel : discord.VoiceChannel = self.bot.get_channel(self.offline_channel_id)
-        self.message_channel : discord.VoiceChannel = self.bot.get_channel(self.message_channel_id)
-        self.time_channel : discord.VoiceChannel = self.bot.get_channel(self.time_channel_id)
+        self.all_channel : discord.VoiceChannel = self.bot.get_channel(663297143909515274)
+        self.member_channel : discord.VoiceChannel = self.bot.get_channel(663297196531253249)
+        self.bot_channel : discord.VoiceChannel = self.bot.get_channel(663297233453842452)
+        self.online_channel : discord.VoiceChannel = self.bot.get_channel(663297268455309332)
+        self.idle_channel : discord.VoiceChannel = self.bot.get_channel(664160147886833678)
+        self.dnd_channel : discord.VoiceChannel = self.bot.get_channel(664160201125003295)
+        self.offline_channel : discord.VoiceChannel = self.bot.get_channel(663297305847398421)
+        self.message_channel : discord.VoiceChannel = self.bot.get_channel(663297421417119754)
+        self.time_channel : discord.VoiceChannel = self.bot.get_channel(663297453621116988)
         # await self.all_channel.edit(name=f"all : {datas["all"]}")
         # await self.member_channel.edit(name=f"member : {datas["member"]}")
         # await self.bot_channel.edit(name=f"bot : {datas["bot"]}")
