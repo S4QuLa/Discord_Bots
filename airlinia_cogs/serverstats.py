5d9301e0885f0b6275a54c56bfef5ee77ee7d10a
amazakura <amazakura0804@gmail.com>
2020-01-10 06:23:02 +0900

    修正

--- airlinia_cogs/serverstats.py
+++ airlinia_cogs/serverstats.py
@@ -20,6 +20,8 @@ class Server_Stats(commands.Cog):
         datas["bot"] = len([member for member in server.members if member.bot])
         with open("./data/pokemon.json", "w") as f:
             json.dump(datas, f, indent=4)
+        with open('./data/stats.json', 'r') as f:
+            datas = json.load(f)
         await self.bot.get_channel(663297143909515274).edit(name=f"all : {datas["all"]}")
         await self.bot.get_channel(663297196531253249).edit(name=f"member : {datas["member"]}")
         await self.bot.get_channel(663297233453842452).edit(name=f"bot : {datas["bot"]}")
@@ -34,6 +36,8 @@ class Server_Stats(commands.Cog):
         datas["message"] += 1
         with open("./data/pokemon.json", "w") as f:
             json.dump(datas, f, indent=4)
+        with open('./data/stats.json', 'r') as f:
+            datas = json.load(f)
         await self.bot.get_channel(663297421417119754).edit(name=f"message : {datas["message"]}")
 
     @commands.Cog.listener()
@@ -47,6 +51,8 @@ class Server_Stats(commands.Cog):
         datas["offline"] = len([member for member in server.members if member.status.offline])
         with open("./data/pokemon.json", "w") as f:
             json.dump(datas, f, indent=4)
+        with open('./data/stats.json', 'r') as f:
+            datas = json.load(f)
         await self.bot.get_channel(663297268455309332).edit(name=f"online : {datas["online"]}")
         await self.bot.get_channel(664160147886833678).edit(name=f"idle : {datas["idle"]}")
         await self.bot.get_channel(664160201125003295).edit(name=f"dnd : {datas["dnd"]}")
