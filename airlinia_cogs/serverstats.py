23540e1ab4274c2239fb408fd50946610463327d
amazakura <amazakura0804@gmail.com>
2020-01-10 05:48:50 +0900

    修正

--- airlinia_cogs/serverstats.py
+++ airlinia_cogs/serverstats.py
@@ -49,7 +49,7 @@ class Server_Stats(commands.Cog):
     async def channel_name_edit():
         with open('./data/stats.json', 'r') as f:
             datas = json.load(f)
-       print(datas)
+        print(datas)
         # await self.bot.get_channel(663297143909515274).edit(name=f"all : {datas["all"]}")
         # await self.bot.get_channel(663297196531253249).edit(name=f"member : {datas["member"]}")
         # await self.bot.get_channel(663297233453842452).edit(name=f"bot : {datas["bot"]}")
