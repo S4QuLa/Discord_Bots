befadd1da42e535b85821505bb7465abf6b8b4ad
amazakura <amazakura0804@gmail.com>
2020-04-25 02:25:27 +0900

    ミニミニ修正～

--- airlinia_cogs/shorturl.py
+++ airlinia_cogs/shorturl.py
@@ -10,10 +10,10 @@ class Short_Url(commands.Cog):
     def __init__(self, airlinia):
         self.bot = airlinia
 
-    @commands.command()
-    async def url(self, ctx, url1, pass):
-        obj = kutt.submit(os.environ['KUTT_API_KEY'], url1, password=pass, customurl="xn--gk8h.ml")
-        await message.channel.send(f"短縮URLを作成しました！{obj['address']}") # 返信メッセージを送信
+    @commands.command(name='url')
+    async def _url(self, ctx, url, pass):
+        obj = kutt.submit(os.environ['KUTT_API_KEY'], url, password=pass, customurl="xn--gk8h.ml")
+        await ctx.send(f"短縮URLを作成しました！{obj['address']}") # 返信メッセージを送信
 
 def setup(airlinia):
     airlinia.add_cog(Short_Url(airlinia))
