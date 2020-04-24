09e15e62400eba39dc15865cf44aecd3af15a821
amazakura <amazakura0804@gmail.com>
2020-04-25 03:05:39 +0900

    customdomainが重複してたンゴｗｗｗｗkuttAPI用済みじゃ消え失せろ

--- airlinia_cogs/shorturl.py
+++ airlinia_cogs/shorturl.py
@@ -18,9 +18,18 @@ class Short_Url(commands.Cog):
         parser.add_argument('url')
         parser.add_argument('--password', '-p')
         parser.add_argument('--customurl', '-c')
+        parser.add_argument('--domain', '-d', default='xn--gk8h.ml')
         args = parser.parse_args()
-        obj = kutt.submit(os.environ['KUTT_API_KEY'], args.url, customurl=args.customurl, password=args.password, customurl="xn--gk8h.ml")
-        await ctx.send(f"短縮URLを作成しました！{obj['address']}") # 返信メッセージを送信
+        payload = {}
+        payload['target'] = args.url
+        payload['domain'] = args.domain
+        if customurl:
+            payload['customurl'] = args.customurl
+        if password:
+            payload['password'] = args.password
+        res = requests.post(BASE_URL+'/api/v2/links', data=payload, headers=headers)
+        data = res.json()
+        await ctx.send(f"短縮URLを作成しました！{data['address']}") # 返信メッセージを送信
 
 def setup(airlinia):
     airlinia.add_cog(Short_Url(airlinia))
