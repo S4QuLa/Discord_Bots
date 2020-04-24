74a90d95b1b4174b6b8c242ec405f77624e9dfb0
amazakura <amazakura0804@gmail.com>
2020-04-25 02:02:04 +0900

    短縮URL作成機能

---
+++ airlinia_cogs/shorturl.py
@@ -0,0 +1,20 @@
+# Discord.py is smoooooooooooooosh!!!!!
+import discord
+from discord.ext import tasks, commands
+import asyncio
+
+import os
+from kutt import kutt
+
+class Short_Url(commands.Cog):
+    def __init__(self, airlinia):
+        self.bot = airlinia
+
+    @commands.command()
+    async def url(self, url1, pass):
+        obj = kutt.submit(os.environ['KUTT_API_KEY'], url1, password=pass, customurl="xn--gk8h.ml")
+        content = obj['address']
+        await message.channel.send(f"短縮URLを作成しました！{content}") # 返信メッセージを送信
+
+def setup(airlinia):
+    airlinia.add_cog(Short_Url(airlinia))
