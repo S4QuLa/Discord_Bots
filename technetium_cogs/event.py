95c67987466b32f992ef97b51118d4e13c51396c
amazakura <amazakura0804@gmail.com>
2020-04-27 16:11:51 +0900

    ウェルカム画像

--- technetium_cogs/event.py
+++ technetium_cogs/event.py
@@ -9,6 +9,8 @@ import random
 class Event(commands.Cog):
     def __init__(self, technetium):
         self.bot = technetium #botを受け取る。
+        self.font_path1 = "./fonts/NotoSansCJKjp-Medium.otf"
+        self.font_path2 = "./fonts/Harenosora.otf"
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
@@ -30,17 +32,59 @@ class Event(commands.Cog):
             color=0xff0000)
             await self.bot.get_channel(596668583728119809).send(embed=embed, delete_after=180)
 
+    def base_image(self, icon_path):
+        icon_size = 380
+        mask = Image.new("L", (icon_size, icon_size), 0)
+        draw = ImageDraw.Draw(mask)
+        draw.ellipse((0, 0, icon_size, icon_size), fill=255)
+        mask = mask.filter(ImageFilter.GaussianBlur(1))
+
+        icon_path = 'icon_path'
+        icon = Image.open(icon_path).copy().convert("RGBA")
+        icon = icon.resize(size=(icon_size, icon_size), resample=Image.ANTIALIAS)
+        circle = Image.new("RGBA", (icon_size + 10, icon_size + 10), 0)
+        draw = ImageDraw.Draw(circle)
+        draw.ellipse((0, 0, icon_size + 10, icon_size + 10), 'White')
+        circle.paste(icon, (5, 5), mask)
+
+        base_image_path = './image/technetium_welcome.png'
+        base_image = Image.open(base_image_path).copy()
+        base_image.paste(circle, (100, 220), circle)
+        draw = ImageDraw.Draw(base_image)
+        draw.line([(320,608), (1920,608)], 'White', width=5)
+
+    def add_text_to_image(self, img, text, font_path, font_size, font_color, height, width, max_length=1300):
+        position = (width, height)
+        font = ImageFont.truetype(font_path, font_size)
+        draw = ImageDraw.Draw(img)
+        if draw.textsize(text, font=font)[0] > max_length:
+            while draw.textsize(text + '…', font=font)[0] > max_length:
+                text = text[:-1]
+            text = text[:-3] + '… さん！'
+        draw.text(position, text, font_color, font=font)
+        return img
+
     @commands.Cog.listener()
     async def on_member_join(self, member):
+        base_image = base_image(member.avatar_url)
+        _text = []
+        _text.append(["ようこそ！Welcome!", self.font_path1, 130, (255, 255, 255), 190, 530])
+        _text.append([f"{member.name} さん！", self.font_path2, 50, (255, 255, 255), 350, 530])
+        _text.append([f"ようこそ、{member.guild.name}へ！\n最初にルールをお読みください。\nあなたは{len(member.guild.members)}人目のメンバーです！", self.font_path2, 55, (255, 255, 255), 440, 530])
+        for t in _text:
+            base_image = add_text_to_image(base_image, t[0], t[1], t[2], t[3], t[4], t[5])
+
+        file = discord.File(base_image, filename="base_image.png")
         cl = discord.Color(random.randint(0, 0xFFFFFF))
         embed = discord.Embed(title=f"利用規約はここから",
                               description=f"{member.guild.name}へようこそ！{member.mention}さん！\n{len(member.guild.members)}人目の参加者です！\n> 何か困ったことがあればぜひとも運営にメンションをしてください。\n> 基本一人は常駐してます。",
                               url="https://www.elegraph.cf/?page_id=24",
                               color=cl)
-        embed.set_author(name=f"{member.display_name}さんが参加しました～！")
+        embed.set_author(name=f"{member.display_name}さんが参加しました～！", icon_url=member.avatar_url)
         embed.set_footer(text=member.guild.name, icon_url=member.guild.icon_url)
         embed.set_thumbnail(url=member.avatar_url)
-        await self.bot.get_channel(596668568909643817).send(embed=embed)
+        embed.set_image(url="attachment://base_image.png")
+        await self.bot.get_channel(596668568909643817).send(file=file, embed=embed)
 
     @commands.Cog.listener()
     async def on_member_remove(self, member):
